59cca8e4fd95187c615fd1ad4a405ddc2ccdf120
keiigon <tarek.iraqi@gmail.com>
2017-05-14 09:54:29 +0200

    Add files via upload

---
+++ Compiler.Lib/Types/AssemblyInstructions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Lib.Types
+{
+    public enum AssemblyInstructions
+    {
+        None,
+        Load,
+        Store,
+        Add,
+        Subt,
+        Input,
+        Output,
+        Halt,
+        Skipcond,
+        Jump,
+        Clear,
+        AddI,
+        JumpI,
+        JNS
+
+    }
+}
---
+++ Compiler.Lib/Types/AssemblyLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Lib.Types
+{
+    public class AssemblyLine
+    {
+        public string label { set; get; }
+        public AssemblyInstructions code { set; get; }
+        public string operand { set; get; }
+
+        public override string ToString()
+        {
+            StringBuilder st = new StringBuilder();
+
+            st.Append(string.IsNullOrEmpty(this.label) ? "" : this.label + ", ");
+            st.Append(this.code.Equals(AssemblyInstructions.None) ? "" : this.code + " ");
+            st.Append(string.IsNullOrEmpty(this.operand) ? "" : this.operand);
+
+            return st.ToString();
+        }
+
+
+        //comment
+    }
+}
